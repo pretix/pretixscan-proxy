2cb7effa9d9fa5279df2d8cfd27551ca9083e24f
Raphael Michel <mail@raphaelmichel.de>
2019-07-19 20:38:27 +0200

    Move function

---
+++ server/src/main/kotlin/eu/pretix/pretixscan/scanproxy/Database.kt
@@ -0,0 +1,80 @@
+package eu.pretix.pretixscan.scanproxy
+
+import eu.pretix.pretixscan.scanproxy.db.Migrations
+import io.requery.Persistable
+import io.requery.cache.EntityCacheBuilder
+import io.requery.sql.ConfigurationBuilder
+import io.requery.sql.EntityDataStore
+import io.requery.sql.KotlinConfiguration
+import io.requery.sql.KotlinEntityDataStore
+import org.postgresql.ds.PGSimpleDataSource
+import org.slf4j.LoggerFactory
+import java.sql.DriverManager
+import eu.pretix.libpretixsync.db.Migrations as PSMigrations
+import eu.pretix.libpretixsync.db.Models as PSModels
+
+internal fun makeSyncDataStore(): EntityDataStore<Persistable> {
+    val LOG = LoggerFactory.getLogger(Server::class.java)
+
+    // TODO: Support other databases
+    val conn = DriverManager.getConnection(System.getProperty("pretixscan.database"))
+    var exists = false
+    val r = conn.metaData.getTables(null, null, "_version", arrayOf("TABLE"))
+    while (r.next()) {
+        if (r.getString("TABLE_NAME") == "_version") {
+            exists = true;
+            break;
+        }
+    }
+    if (!exists) {
+        LOG.info("Creating new database.")
+    }
+
+    val dataSource = PGSimpleDataSource()
+    dataSource.setURL(System.getProperty("pretixscan.database"))
+    val model = PSModels.DEFAULT
+    PSMigrations.migrate(dataSource, !exists)
+    val configuration = ConfigurationBuilder(dataSource, model)
+        .setEntityCache(
+            EntityCacheBuilder(model)
+                .useReferenceCache(false)
+                .useSerializableCache(false)
+                .build()
+        )
+        .build()
+
+    return EntityDataStore<Persistable>(configuration)
+}
+
+
+internal fun makeProxyDataStore(): KotlinEntityDataStore<Persistable> {
+    val LOG = LoggerFactory.getLogger(Server::class.java)
+
+    // TODO: Support other databases
+    val conn = DriverManager.getConnection(System.getProperty("pretixscan.database"))
+    var exists = false
+    val r = conn.metaData.getTables(null, null, "_scanproxy_version", arrayOf("TABLE"))
+    while (r.next()) {
+        if (r.getString("TABLE_NAME") == "_scanproxy_version") {
+            exists = true;
+            break;
+        }
+    }
+    if (!exists) {
+        LOG.info("Creating new database.")
+    }
+
+    val dataSource = PGSimpleDataSource()
+    dataSource.setURL(System.getProperty("pretixscan.database"))
+    val model = Models.DEFAULT
+    Migrations.migrate(dataSource, !exists)
+    val configuration = KotlinConfiguration(
+        dataSource = dataSource,
+        model = model,
+        cache = EntityCacheBuilder(model)
+            .useReferenceCache(false)
+            .useSerializableCache(false)
+            .build()
+    )
+    return KotlinEntityDataStore(configuration = configuration)
+}
--- server/src/main/kotlin/eu/pretix/pretixscan/scanproxy/ProxyServer.kt
+++ server/src/main/kotlin/eu/pretix/pretixscan/scanproxy/ProxyServer.kt
@@ -1,8 +1,6 @@
 package eu.pretix.pretixscan.scanproxy
 
 import com.fasterxml.jackson.databind.module.SimpleModule
-import eu.pretix.pretixscan.scanproxy.db.Migrations
-import eu.pretix.pretixscan.scanproxy.Models
 import eu.pretix.pretixscan.scanproxy.endpoints.SetupDownstream
 import eu.pretix.pretixscan.scanproxy.endpoints.SetupDownstreamInit
 import eu.pretix.pretixscan.scanproxy.endpoints.SetupUpstream
@@ -13,88 +11,11 @@ import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.path
 import io.javalin.apibuilder.ApiBuilder.post
 import io.javalin.plugin.json.JavalinJackson
-import io.requery.Persistable
-import io.requery.cache.EntityCacheBuilder
-import io.requery.sql.ConfigurationBuilder
-import io.requery.sql.EntityDataStore
-import io.requery.sql.KotlinConfiguration
-import io.requery.sql.KotlinEntityDataStore
 import net.harawata.appdirs.AppDirsFactory
 import org.json.JSONArray
 import org.json.JSONObject
-import org.postgresql.ds.PGSimpleDataSource
 import org.slf4j.LoggerFactory
-import java.sql.DriverManager
-import eu.pretix.libpretixsync.db.Migrations as PSMigrations
-import eu.pretix.libpretixsync.db.Models as PSModels
 
-
-private fun makeSyncDataStore(): EntityDataStore<Persistable> {
-    val LOG = LoggerFactory.getLogger(Server::class.java)
-
-    // TODO: Support other databases
-    val conn = DriverManager.getConnection(System.getProperty("pretixscan.database"))
-    var exists = false
-    val r = conn.metaData.getTables(null, null, "_version", arrayOf("TABLE"))
-    while (r.next()) {
-        if (r.getString("TABLE_NAME") == "_version") {
-            exists = true;
-            break;
-        }
-    }
-    if (!exists) {
-        LOG.info("Creating new database.")
-    }
-
-    val dataSource = PGSimpleDataSource()
-    dataSource.setURL(System.getProperty("pretixscan.database"))
-    val model = PSModels.DEFAULT
-    PSMigrations.migrate(dataSource, !exists)
-    val configuration = ConfigurationBuilder(dataSource, model)
-        // .useDefaultLogging()
-        .setEntityCache(
-            EntityCacheBuilder(model)
-                .useReferenceCache(false)
-                .useSerializableCache(false)
-                .build()
-        )
-        .build()
-
-    return EntityDataStore<Persistable>(configuration)
-}
-
-
-private fun makeProxyDataStore(): KotlinEntityDataStore<Persistable> {
-    val LOG = LoggerFactory.getLogger(Server::class.java)
-
-    // TODO: Support other databases
-    val conn = DriverManager.getConnection(System.getProperty("pretixscan.database"))
-    var exists = false
-    val r = conn.metaData.getTables(null, null, "_scanproxy_version", arrayOf("TABLE"))
-    while (r.next()) {
-        if (r.getString("TABLE_NAME") == "_scanproxy_version") {
-            exists = true;
-            break;
-        }
-    }
-    if (!exists) {
-        LOG.info("Creating new database.")
-    }
-
-    val dataSource = PGSimpleDataSource()
-    dataSource.setURL(System.getProperty("pretixscan.database"))
-    val model = Models.DEFAULT
-    Migrations.migrate(dataSource, !exists)
-    val configuration = KotlinConfiguration(
-        dataSource = dataSource,
-        model = model,
-        cache = EntityCacheBuilder(model)
-            .useReferenceCache(false)
-            .useSerializableCache(false)
-            .build()
-    )
-    return KotlinEntityDataStore(configuration = configuration)
-}
 
 object Server {
     val VERSION = "0.0.1"
