308dbf60874abb5e89683f3ee7b825c2e99133e6
Raphael Michel <michel@rami.io>
2022-07-06 09:31:55 +0200

    Bump to 1.15.0

--- server/src/main/kotlin/eu/pretix/pretixscan/scanproxy/ProxyServer.kt
+++ server/src/main/kotlin/eu/pretix/pretixscan/scanproxy/ProxyServer.kt
@@ -17,8 +17,8 @@ import java.util.concurrent.Executors
 
 
 object Server {
-    val VERSION = "1.14.2"
-    val VERSION_CODE = 7
+    val VERSION = "1.15.0"
+    val VERSION_CODE = 8
     private val LOG = LoggerFactory.getLogger(Server::class.java)
     val syncData = makeSyncDataStore()
     val proxyData = makeProxyDataStore()
