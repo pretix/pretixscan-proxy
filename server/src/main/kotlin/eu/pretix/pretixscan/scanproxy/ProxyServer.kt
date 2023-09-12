a8f390ba3068d235346e0fea5cea53353473cd7e
Raphael Michel <michel@rami.io>
2023-09-12 10:51:01 +0200

    Bump to 2.3.0

--- server/src/main/kotlin/eu/pretix/pretixscan/scanproxy/ProxyServer.kt
+++ server/src/main/kotlin/eu/pretix/pretixscan/scanproxy/ProxyServer.kt
@@ -18,8 +18,8 @@ import java.util.concurrent.Executors
 
 
 object Server {
-    val VERSION = "2.2.0"
-    val VERSION_CODE = 11
+    val VERSION = "2.3.0"
+    val VERSION_CODE = 12
     private val LOG = LoggerFactory.getLogger(Server::class.java)
     val syncData = makeSyncDataStore()
     val proxyData = makeProxyDataStore()
