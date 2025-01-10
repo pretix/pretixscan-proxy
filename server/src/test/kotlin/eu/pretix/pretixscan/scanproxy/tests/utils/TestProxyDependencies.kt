ce6d2b9b54538669fd533791716bef53734ac3d8
Anton Weber <git@anonweb.de>
2025-01-10 19:12:43 +0100

    Update TestProxyDependencies

--- server/src/test/kotlin/eu/pretix/pretixscan/scanproxy/tests/utils/TestProxyDependencies.kt
+++ server/src/test/kotlin/eu/pretix/pretixscan/scanproxy/tests/utils/TestProxyDependencies.kt
@@ -3,19 +3,17 @@ package eu.pretix.pretixscan.scanproxy.tests.utils
 import eu.pretix.libpretixsync.api.HttpClientFactory
 import eu.pretix.libpretixsync.api.PretixApi
 import eu.pretix.libpretixsync.api.RateLimitInterceptor
-import eu.pretix.pretixscan.scanproxy.Models
 import eu.pretix.pretixscan.scanproxy.ProxyDependencies
 import eu.pretix.pretixscan.scanproxy.ProxyScanConfig
-import eu.pretix.pretixscan.scanproxy.db.Migrations
+import eu.pretix.pretixscan.scanproxy.db.createProxyDatabase
 import eu.pretix.pretixscan.scanproxy.db.createSyncDatabase
-import eu.pretix.pretixscan.scanproxy.sqldelight.SyncDatabase
+import eu.pretix.pretixscan.scanproxy.sqldelight.proxy.ProxyDatabase
+import eu.pretix.pretixscan.scanproxy.sqldelight.sync.SyncDatabase
 import eu.pretix.pretixscan.scanproxy.tests.test.FakePretixApi
 import io.requery.Persistable
 import io.requery.cache.EntityCacheBuilder
 import io.requery.sql.ConfigurationBuilder
 import io.requery.sql.EntityDataStore
-import io.requery.sql.KotlinConfiguration
-import io.requery.sql.KotlinEntityDataStore
 import okhttp3.OkHttpClient
 import org.postgresql.ds.PGSimpleDataSource
 import org.slf4j.LoggerFactory
@@ -24,35 +22,7 @@ import java.sql.DriverManager
 import java.util.concurrent.TimeUnit
 import kotlin.io.path.absolutePathString
 
-class TestProxyDependencies() : ProxyDependencies() {
-
-    override val proxyData: KotlinEntityDataStore<Persistable> by lazy {
-        val url = System.getProperty("pretixscan.database")
-
-        val conn = DriverManager.getConnection(url)
-        var exists = false
-        val r = conn.metaData.getTables(null, null, "_scanproxy_version", arrayOf("TABLE"))
-        while (r.next()) {
-            if (r.getString("TABLE_NAME") == "_scanproxy_version") {
-                exists = true
-                break
-            }
-        }
-
-        val dataSource = PGSimpleDataSource()
-        dataSource.setURL(url)
-        val model = Models.DEFAULT
-        Migrations.migrate(dataSource, !exists)
-        val configuration = KotlinConfiguration(
-            dataSource = dataSource,
-            model = model,
-            cache = EntityCacheBuilder(model)
-                .useReferenceCache(false)
-                .useSerializableCache(false)
-                .build()
-        )
-        KotlinEntityDataStore(configuration = configuration)
-    }
+class TestProxyDependencies : ProxyDependencies() {
 
     override val syncData: EntityDataStore<Persistable> by lazy {
         val url = System.getProperty("pretixscan.database")
@@ -112,6 +82,15 @@ class TestProxyDependencies() : ProxyDependencies() {
         super.init()
         System.setProperty("pretixscan.adminauth", "foo:bar")
     }
+
+    override val proxyDb: ProxyDatabase
+        get() {
+            val LOG = LoggerFactory.getLogger(TestProxyDependencies::class.java)
+            return createProxyDatabase(
+                url = System.getProperty("pretixscan.database"),
+                LOG = LOG,
+            )
+        }
 
     override val db: SyncDatabase
         get() {
